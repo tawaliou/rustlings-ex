6bc78fbe885e8880566f4cfb19c0cd072e57b1dc
TawalMc <developtawaliou@gmail.com>
2026-01-13 12:38:39 +0100

    complete enums exercises

--- exercises/08_enums/enums1.rs
+++ exercises/08_enums/enums1.rs
@@ -1,9 +1,21 @@
 #[derive(Debug)]
 enum Message {
     // TODO: Define a few types of messages as used below.
+	Resize,
+	Move,
+	Echo,
+	ChangeColor,
+	Quit
 }
 
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+
 fn main() {
+
     println!("{:?}", Message::Resize);
     println!("{:?}", Message::Move);
     println!("{:?}", Message::Echo);
