2ffa3d1b9067b418ec4133b76e219194078a5cfa
Tawaliou ALAO <developtawaliou@gmail.com>
2026-04-02 20:29:52 +0100

    complete errors, generics, traits exercises

--- exercises/15_traits/traits1.rs
+++ exercises/15_traits/traits1.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 // The trait `AppendBar` has only one function which appends "Bar" to any object
 // implementing this trait.
 trait AppendBar {
@@ -6,6 +8,9 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for the type `String`.
+	fn append_bar(self) -> Self {
+		self.add("Bar")
+	}
 }
 
 fn main() {
